512461c25d10b4ea6897cf2867e5d19066a55f91
ioanamoflic <moflicioana@gmail.com>
2024-09-24 17:52:44 +0300

    Multi-threaded verification

--- benchmarking/verification.py
+++ benchmarking/verification.py
@@ -23,15 +23,15 @@ else:
     print("Connection to the PostgreSQL encountered and error.")
 
 
-def concatenate(initial_circuit, cnt):
+def concatenate(init_circ, cnt, bernoulli_percentage=10):
     create_linked_table(conn=connection, clean=True)
     refresh_all_stored_procedures(conn=connection)
 
-    db_tuples, _ = cirq_to_db(cirq_circuit=initial_circuit, last_id=0, add_margins=True, label='verification')
+    db_tuples, _ = cirq_to_db(cirq_circuit=init_circ, last_id=0, add_margins=True, label='verification')
     insert_in_batches(db_tuples=db_tuples, conn=connection, reset_id=10000000)
 
     thread_procedures = [
-        (1, f"call linked_cx_to_hhcxhh_bernoulli(50, {cnt})"),
+        (1, f"call linked_cx_to_hhcxhh_bernoulli({bernoulli_percentage}, {cnt})"),
     ]
 
     db_multi_threaded(thread_proc=thread_procedures)
@@ -42,27 +42,34 @@ def concatenate(initial_circuit, cnt):
     extracted_circuit_reversed = extracted_circuit[::-1]
     qubit_map = dict(
         zip(
-            sorted(initial_circuit.all_qubits()),
+            sorted(init_circ.all_qubits()),
             sorted(extracted_circuit_reversed.all_qubits())
         )
     )
-    initial_circuit = initial_circuit.transform_qubits(qubit_map=qubit_map)
-    concatenated_circuit = cirq.Circuit(initial_circuit.moments + extracted_circuit_reversed.moments)
+    init_circ = init_circ.transform_qubits(qubit_map=qubit_map)
+    concatenated_circuit = cirq.Circuit(init_circ.moments + extracted_circuit_reversed.moments)
 
     return concatenated_circuit
 
 
-def verify_C_Ct_eq_I(concatenated_circuit, cnt):
+def verify_C_Ct_eq_I(concatenated_circuit, cnt, bernoulli_percentage, single_threaded=False):
     create_linked_table(conn=connection, clean=True)
     refresh_all_stored_procedures(conn=connection)
 
     db_tuples, _ = cirq_to_db(cirq_circuit=concatenated_circuit, last_id=0, add_margins=True, label='verification')
     insert_in_batches(db_tuples=db_tuples, conn=connection, reset_id=10000000)
 
-    thread_procedures = [
-        (1, f"call linked_hhcxhh_to_cx_bernoulli(50, {cnt})"),
-        (1, f"call cancel_two_qubit_bernoulli('CXPowGate', 'CXPowGate', 50, {cnt})"),
-    ]
+    if single_threaded is True:
+        thread_procedures = [
+            (1, f"call linked_hhcxhh_to_cx_bernoulli({bernoulli_percentage}, {cnt})"),
+            (1, f"call cancel_two_qubit_bernoulli('CXPowGate', 'CXPowGate', {bernoulli_percentage}, {cnt})"),
+        ]
+    else:
+        thread_procedures = []
+        for _ in range(cnt):
+            thread_procedures.append((1, f"call linked_hhcxhh_to_cx_bernoulli({bernoulli_percentage}, 1)"))
+            thread_procedures.append((1, f"call cancel_two_qubit_bernoulli('CXPowGate', 'CXPowGate', "
+                                         f"{bernoulli_percentage}, 1)"))
 
     db_multi_threaded(thread_proc=thread_procedures)
     extracted_circuit = extract_cirq_circuit(conn=connection,
@@ -74,11 +81,19 @@ def verify_C_Ct_eq_I(concatenated_circuit, cnt):
 
 if __name__ == "__main__":
     times = []
-    for cx_count in range(2, 100):
-        initial_circuit = generate_random_CX_circuit(n_templates=cx_count, n_qubits=cx_count)
-        concatenated = concatenate(initial_circuit=initial_circuit, cnt=cx_count)
+    single_thr = True
+    bp = 25
+    for cx_count in range(2, 50):
+        initial_circuit = generate_random_CX_circuit(n_templates=cx_count,
+                                                     n_qubits=cx_count)
+        concatenated = concatenate(init_circ=initial_circuit,
+                                   cnt=cx_count,
+                                   bernoulli_percentage=bp)
         start_time = time.time()
-        verify_C_Ct_eq_I(concatenated_circuit=concatenated, cnt=cx_count)
+        verify_C_Ct_eq_I(concatenated_circuit=concatenated,
+                         cnt=cx_count,
+                         single_threaded=single_thr,
+                         bernoulli_percentage=bp)
         times.append((cx_count, time.time() - start_time))
         print(cx_count)
 
