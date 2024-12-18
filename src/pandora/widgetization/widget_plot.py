fc1437e812f032ac7c4756a60e4d0b95a70b243f
alexandrupaler <alexandrupaler@gmail.com>
2024-12-18 12:51:42 +0200

    Update widget_plot.py

--- src/pandora/widgetization/widget_plot.py
+++ src/pandora/widgetization/widget_plot.py
@@ -16,14 +16,14 @@ def plot3dsurface():
     y = df['record_t']
     z = df['widget_count']
 
-    x = np.log(x)
-    y = np.log(y)
-    z = np.log(z)
-
+    x = np.log10(x)
+    y = np.log10(y)
+    z = np.log10(z)
+    
     surf = ax.plot_trisurf(x, y, z, antialiased=False, edgecolor="black", linewidth=0.1,)
 
-    ax.set_xlabel("Depth")
-    ax.set_ylabel("T-count")
-    ax.set_zlabel("Widget-count")
+    ax.set_xlabel("Log(Depth)")
+    ax.set_ylabel("Log(T-count)")
+    ax.set_zlabel("Log(Widget-Count)")
 
     plt.show()
