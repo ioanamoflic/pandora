5eb00c0e649cdcc8a85af574e454196275ea94bc
alexandru paler <alexandrupaler@gmail.com>
2025-01-26 23:07:21 +0200

    dummy json files that is generated by lscom

--- pandora_minetest_server.py
+++ pandora_minetest_server.py
@@ -4,13 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
+
 @app.route('/get_nr_slices')
 def get_nr_slices():
     return "1"
+
 
 @app.route('/get_slice/<nr>')
 def get_slice(nr):
@@ -18,9 +21,9 @@ def get_slice(nr):
         d = json.load(f)
         return d
 
+
 # main driver function
 if __name__ == '__main__':
-
     # run() method of Flask class runs the application
     # on the local development server.
     app.run()
