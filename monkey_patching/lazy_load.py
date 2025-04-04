94f92e9f0bbcf20da4a82266c97b35a1a07eef93
ioanamoflic <moflicioana@gmail.com>
2025-04-04 18:15:38 +0300

    Python is a pain -- make_fh_circuit now lazily instantiating QSVT_real_polynomial

--- monkey_patching/lazy_load.py
+++ monkey_patching/lazy_load.py
@@ -1,4 +1,4 @@
-import pyLIQTR.qubitization.qsvt
+from pyLIQTR.qubitization.qsvt import QSVT_real_polynomial
 
 
 class LazyProxy:
@@ -10,8 +10,14 @@ class LazyProxy:
     def _instantiate(self):
         if self._instance is None:
             print(f"Lazily instantiating QSVT_real_polynomial...")
-            self._instance = pyLIQTR.qubitization.qsvt.QSVT_real_polynomial(*self._args, **self._kwargs)
+            self._instance = QSVT_real_polynomial(*self._args, **self._kwargs)
 
     def __getattr__(self, name):
         self._instantiate()
         return getattr(self._instance, name)
+
+
+# monkey-patch
+print("Monkey-patching")
+lazy_QSVT_real_polynomial = lambda *args, **kwargs: LazyProxy(*args, **kwargs)
+
--- patcher_test.py
+++ patcher_test.py
@@ -1,30 +1,11 @@
-from pyLIQTR.BlockEncodings import VALID_ENCODINGS
-from pyLIQTR.BlockEncodings.getEncoding import getEncoding
-from pyLIQTR.ProblemInstances.getInstance import getInstance
-from pyLIQTR.clam.lattice_definitions import SquareLattice
-from pyLIQTR.qubitization.qsvt_dynamics import simulation_phases
+import pyLIQTR.qubitization.qsvt
+from monkey_patching.lazy_load import lazy_QSVT_real_polynomial
+pyLIQTR.qubitization.qsvt.QSVT_real_polynomial = lazy_QSVT_real_polynomial
 
-from monkey_patching.lazy_load import *
 
-# monkey-patch
-print("Monkey-patching")
-QSVT_real_polynomial = lambda *args, **kwargs: LazyProxy(*args, **kwargs)
+from pandora.pyliqtr_to_pandora_util import make_fh_circuit
 
-p_algo = 0.95
-times = 0.1
-J = -1.0
-U = 2.0
-N = 2
-eps = (1 - p_algo) / 2
-model = getInstance("FermiHubbard", shape=(N, N), J=J, U=U, cell=SquareLattice)
-scaled_times = times * model.alpha
-phases = simulation_phases(times=scaled_times, eps=eps, precompute=False, phase_algorithm="random")
+print("Trying to build FH circuit, this should first lazily instantiate QSVT_real_polynomial")
+proc_circuit = make_fh_circuit(N=2, p_algo=0.9999999904, times=0.01)
 
-# this should not call the constructor of GateOperation
-print("Declaring obj, not yet instantiating")
-bloq = QSVT_real_polynomial(block_encoding=getEncoding(VALID_ENCODINGS.PauliLCU),
-                            instance=model,
-                            phis=phases[0])
-# the constructor is called now
-print("Accessing member of QSVT_real_polynomial")
-print(bloq.decompose_from_registers(context=None))
+
