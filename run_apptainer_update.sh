250a9c4cc428cd72ea8b2e2a6d16d508b20c0246
Ioana Moflic <moflicioana@gmail.com>
2025-05-08 11:10:05 +0000

    Latest postgres server config

--- run_apptainer_update.sh
+++ run_apptainer_update.sh
@@ -33,30 +33,62 @@ echo 'host all all 0.0.0.0/0 trust'>> /var/lib/postgresql/data/pg_hba.conf
 
 pg_ctl -o \"$INITDBPORT\" start
 
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set shared_buffers = '400GB';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set work_mem = '64MB';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set maintenance_work_mem = '4GB';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set checkpoint_completion_target = '0.9';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set checkpoint_timeout = '3600s';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set wal_buffers = '1GB';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set max_wal_size = '32GB';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set wal_writer_delay = '10ms';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set synchronous_commit = 'off';\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"alter system set autovacuum_naptime = '10s';\"
+# memory
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET shared_buffers = '512GB';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET work_mem = '64MB';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET maintenance_work_mem = '16GB';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET effective_cache_size = '1200GB';\"
+
+# wal
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET wal_buffers = '512MB';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET wal_writer_delay = '5ms';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET commit_delay = '0';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET commit_siblings = '5';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET synchronous_commit = 'off';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET fsync = 'on';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET full_page_writes = 'off';\"
+
+# checkpoint
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET checkpoint_timeout = '30min';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET checkpoint_completion_target = '0.9';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_wal_size = '128GB';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET min_wal_size = '32GB';\"
+
+# autovacuum
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum = 'on';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_max_workers = '64';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_naptime = '2s';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_cost_limit = '8000';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_cost_delay = '1ms';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_threshold = '100';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_scale_factor = '0.005';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_insert_threshold = '1000';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET autovacuum_vacuum_insert_scale_factor = '0.01';\"
+
+# workers
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_connections = '512';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET superuser_reserved_connections = '10';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_worker_processes = '192';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_parallel_workers = '128';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_parallel_workers_per_gather = '16';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET parallel_leader_participation = 'on';\"
+
+# locks
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET max_locks_per_transaction = '256';\"
+
+# i/o
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET effective_io_concurrency = '512';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET random_page_cost = '1.0';\"
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET track_io_timing = 'on';\"
+
+# logging
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET log_checkpoints = 'on';\"
+
+# temp
+psql -U $USER -d $DB_NAME -p $PORT -c \"ALTER SYSTEM SET temp_buffers = '64MB';\"
 
 
 pg_ctl -o \"$INITDBPORT\" restart
-
-psql -U $USER -d $DB_NAME -p $PORT -c \"show shared_buffers;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show work_mem;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show maintenance_work_mem;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show checkpoint_completion_target;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show checkpoint_timeout;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show max_wal_size;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show wal_buffers;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show wal_writer_delay;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show synchronous_commit;\"
-psql -U $USER -d $DB_NAME -p $PORT -c \"show autovacuum_naptime;\"
 
 export PYTHONPATH=$PYTHONPATH:/pandora/src:/pandora
 
