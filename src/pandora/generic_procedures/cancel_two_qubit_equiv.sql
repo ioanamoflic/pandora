6c2bcd47fbd84485d9be150a6c62693b58b60c3c
ioanamoflic <moflicioana@gmail.com>
2025-07-20 13:49:01 +0300

    Tweak verification param

--- src/pandora/generic_procedures/cancel_two_qubit_equiv.sql
+++ src/pandora/generic_procedures/cancel_two_qubit_equiv.sql
@@ -26,7 +26,7 @@ begin
 --     insert into max_missed_rounds values (0);
 	while run_nr > 0 loop
 
-        if rounds_missed >= 1500 then
+        if rounds_missed >= 5000 then
             exit;
         end if;
 
