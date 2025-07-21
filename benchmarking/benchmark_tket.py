3505d0795cf6404ae7625c0f5334bb8e706b1616
ioanamoflic <moflicioana@gmail.com>
2025-07-21 14:06:29 +0300

    Multithreaded benchmarks

--- benchmarking/benchmark_tket.py
+++ benchmarking/benchmark_tket.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
                 op_time = test_cx_to_hhcxhh_visit_all(connection=conn,
                                                       initial_circuit=pandora_circ,
                                                       nprocs=nprocs,
-                                                      bernoulli_percentage=1000,
+                                                      bernoulli_percentage=100000,
                                                       repetitions=cx_count)
                 print('Pandora time: ', op_time)
             else:
