5e18c0041718b272408c4f733826266f0eb9c8d9
ioanamoflic <moflicioana@gmail.com>
2025-07-22 12:17:23 +0300

    Attempt at deadlock avoidance

--- src/pandora/generic_procedures/cx_to_hhcxhh_batched.sql
+++ src/pandora/generic_procedures/cx_to_hhcxhh_batched.sql
@@ -78,9 +78,9 @@ begin
                     execute 'update linked_circuit set ' || modulus_right_cx_q2 || ' = $1 where id = $2' using right_q2_id, cx_next_q2_id;
                     run_nr = run_nr - 1;
                 end if;
-                commit;
             end if;
         end loop;
+        commit;
     end loop;
 end;$$;
 
