4f009f7732a63e6b4e1b7b1c965d937df3ad3e11
ioanamoflic <moflicioana@gmail.com>
2025-07-22 18:13:40 +0300

    Bugfix adders

--- benchmarking/benchmark_adders.py
+++ benchmarking/benchmark_adders.py
@@ -55,7 +55,7 @@ def get_adder(n_bits: int):
     qubits = [cirq.NamedQubit(f'q{i}') for i in range(3 * n_bits)]
 
     # Adders from https://github.com/njross/optimizer/tree/master/QFT_and_Adders
-    with open(f"adders/Adder{n_bits}.txt", "r") as f:
+    with open(f"benchmarking/adders/Adder{n_bits}.txt", "r") as f:
         for line in f:
             line = line.strip()
             if line.startswith("QGate[\"not\"]"):
