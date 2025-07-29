237bed1c4d34bc43d0de1fdc473163dc50870012
ioanamoflic <moflicioana@gmail.com>
2025-07-30 00:35:14 +0300

    Sequential benchmark ++

--- benchmarking/benchmark_qiskit.py
+++ benchmarking/benchmark_qiskit.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     # in the range 0...100
     sample_percentage = 1
 
-    for nq in range(10000, 100001, 10000):
+    for nq in range(100000, 1000001, 100000):
         print(f'Number of qubits: {nq} for {nr_passes} passes and {sample_percentage} probability')
 
         if DIR == 0:
