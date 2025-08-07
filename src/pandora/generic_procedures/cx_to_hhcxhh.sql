79aa3bd44ef13a23cf34614a922b81120d3af000
alexandru paler <alexandrupaler@gmail.com>
2025-08-07 13:30:35 +0300

    fixed bug in cx2hh

--- src/pandora/generic_procedures/cx_to_hhcxhh.sql
+++ src/pandora/generic_procedures/cx_to_hhcxhh.sql
@@ -23,14 +23,6 @@ declare
 
 	start_time timestamp;
 
---
---     cx record;
---     gate record;
-
-    cx_prev_q1_id bigint;
-	cx_prev_q2_id bigint;
-	cx_next_q1_id bigint;
-	cx_next_q2_id bigint;
     left_h_q1_id bigint;
 	left_h_q2_id bigint;
 	right_h_q1_id bigint;
@@ -41,19 +33,12 @@ declare
     gate_type bigint;
 
     -- the links to the right and left
-    left_q1_link bigint;
-    left_q2_link bigint;
-    right_q1_link bigint;
-    right_q2_link bigint;
-
-    cx_id_ctrl bigint;
-    cx_id_tgt bigint;
-
---     start_time timestamp;
---     a record;
---     b record;
---     c record;
---     d record;
+    first_port0 bigint;
+    first_port1 bigint;
+    first_port2 bigint;
+    second_port0 bigint;
+    second_port1 bigint;
+    second_port2 bigint;
 
 begin
     port_nr := 0; -- single qubit gate has a single port with index 0
@@ -75,13 +60,8 @@ begin
                 continue;
             end if;
 
---             if second.param != param_2 or first.type != type_2 then
---                 commit;
---                 continue;
---             end if;
-
             m11_id := div(first.prev_q1, 1000);
-            m21_id := div(first.prev_q2, 1000);
+            m12_id := div(first.prev_q2, 1000);
             m21_id := div(second.next_q1, 1000);
             m22_id := div(second.next_q2, 1000);
 
@@ -90,73 +70,62 @@ begin
             select * into m21 from linked_circuit where id = m21_id for update skip locked;
             select * into m22 from linked_circuit where id = m22_id for update skip locked;
 
-            if m11.id is null or m21.id is null or m12.id is null or m22.id is null then
+            if m11.id is null or m12.id is null or m21.id is null or m22.id is null then
                 commit;
                 continue;
             end if;
 
---             cx_prev_q1_id := div(cx.prev_q1, 1000);
---             cx_prev_q2_id := div(cx.prev_q2, 1000);
---             cx_next_q1_id := div(cx.next_q1, 1000);
---             cx_next_q2_id := div(cx.next_q2, 1000);
---
---             select * into a from linked_circuit where id = cx_prev_q1_id for update skip locked;
---             select * into b from linked_circuit where id = cx_prev_q2_id for update skip locked;
---             select * into c from linked_circuit where id = cx_next_q1_id for update skip locked;
---             select * into d from linked_circuit where id = cx_next_q2_id for update skip locked;
---
---             -- Lock the 4 neighbours
---             if a.id is null or b.id is null or c.id is null or d.id is null then
---                 commit;
---                 continue;
---             end if;
+            first_port0 := first.id * 1000 + 0 * 100 + first.type;
+            first_port1 := first.id * 1000 + 1 * 100 + first.type;
+            second_port0 := second.id * 1000 + 0 * 100 + second.type;
+            second_port1 := second.id * 1000 + 1 * 100 + second.type;
 
-            cx_id_ctrl := (first.id * 10 + 0) * 100 + first.type;
-            cx_id_tgt  := (first.id * 10 + 1) * 100 + first.type;
-
-            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (first.prev_q1, gate_type, 1, cx_id_tgt, my_proc_id, first.label)
+            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (first.prev_q1, gate_type, 1, first_port1, my_proc_id, first.label)
                                                           returning id into left_h_q1_id;
 
-            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (first.prev_q2, gate_type, 1, cx_id_ctrl, my_proc_id, first.label)
+            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (first.prev_q2, gate_type, 1, first_port0, my_proc_id, first.label)
                                                           returning id into left_h_q2_id;
 
-            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (cx_id_tgt, gate_type, 1, first.next_q1, my_proc_id, first.label)
+            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (second_port1, gate_type, 1, first.next_q1, my_proc_id, first.label)
                                                           returning id into right_h_q1_id;
 
-            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (cx_id_ctrl, gate_type, 1, first.next_q2,my_proc_id, first.label)
+            insert into linked_circuit(prev_q1, type, param, next_q1, visited, label) values (second_port0, gate_type, 1, first.next_q2,my_proc_id, first.label)
                                                           returning id into right_h_q2_id;
 
             -- These are the links that the margins and CNOT should use
-            left_q1_link := left_h_q1_id * 1000 + port_nr * 100 + gate_type;
-            left_q2_link := left_h_q2_id * 1000 + port_nr * 100 + gate_type;
-            right_q1_link := right_h_q1_id * 1000 + port_nr * 100 + gate_type;
-            right_q2_link := right_h_q2_id * 1000 + port_nr * 100 + gate_type;
+            -- First and Second are practically updated because of the new Hadamards
+            -- But we keep them referring to the CX gates
+            -- Update the first and second ports
+            first_port0 := left_h_q1_id * 1000 + port_nr * 100 + gate_type;
+            first_port1 := left_h_q2_id * 1000 + port_nr * 100 + gate_type;
+            second_port0 := right_h_q1_id * 1000 + port_nr * 100 + gate_type;
+            second_port1 := right_h_q2_id * 1000 + port_nr * 100 + gate_type;
 
             -- Update the CNOT: flip the switch value and set visited
-            update linked_circuit set (prev_q1, prev_q2, next_q1, next_q2, switch, visited) = (left_q2_link, left_q1_link, right_q2_link, right_q1_link, not cx.switch, my_proc_id) where id = cx.id;
+            update linked_circuit set (prev_q1, prev_q2, next_q1, next_q2, switch, visited) = (first_port1, first_port0, second_port1, second_port0, not first.switch, my_proc_id) where id = first.id;
 
             if mod(div(first.prev_q1, 100), 10) = 0 then
-                update linked_circuit set next_q1 = left_q1_link where id = m11_id;
+                update linked_circuit set next_q1 = first_port0 where id = m11_id;
             else
-                update linked_circuit set next_q2 = left_q1_link where id = m11_id;
+                update linked_circuit set next_q2 = first_port0 where id = m11_id;
             end if;
 
             if mod(div(first.prev_q2, 100), 10) = 0 then
-                update linked_circuit set next_q1 = left_q2_link where id = m12_id;
+                update linked_circuit set next_q1 = first_port1 where id = m12_id;
             else
-                update linked_circuit set next_q2 = left_q2_link where id = m12_id;
+                update linked_circuit set next_q2 = first_port1 where id = m12_id;
             end if;
 
             if mod(div(second.next_q1, 100), 10) = 0 then
-                update linked_circuit set prev_q1 = right_q1_link where id = m21_id;
+                update linked_circuit set prev_q1 = second_port0 where id = m21_id;
             else
-                update linked_circuit set prev_q2 = right_q1_link where id = m21_id;
+                update linked_circuit set prev_q2 = second_port0 where id = m21_id;
             end if;
 
             if mod(div(second.next_q2, 100), 10) = 0 then
-                update linked_circuit set prev_q1 = right_q2_link where id = m22_id;
+                update linked_circuit set prev_q1 = second_port1 where id = m22_id;
             else
-                update linked_circuit set prev_q2 = right_q2_link where id = m22_id;
+                update linked_circuit set prev_q2 = second_port1 where id = m22_id;
             end if;
 
             commit; -- release locks
