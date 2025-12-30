96c4f002d7a2a8b144b196d4f6246d3b3f17d090
alexandru paler <alexandrupaler@gmail.com>
2025-12-30 17:32:18 +0200

    extracted test reconstruction

--- tests/cirq_to_pandora_test.py
+++ tests/cirq_to_pandora_test.py
@@ -1,3 +1,5 @@
+from qsharp import circuit
+
 from pandora.connection_util import *
 
 from qualtran.bloqs.arithmetic.addition import Add
@@ -8,6 +10,8 @@ from pandora.pandora_util import pandora_to_circuit
 from pandora.qualtran_to_pandora_util import get_cirq_circuit_for_bloq, assert_circuit_is_pandora_ingestible
 
 from pandora.cirq_util import create_random_circuit
+from pyLIQTR.utils.repeat import circuit_to_quregs
+
 
 def get_adder_as_cirq_circuit(n_bits) -> cirq.Circuit:
     """
@@ -63,119 +67,63 @@ def test_random_reconstruction(n_circuits=100):
         print('Test passed!')
 
 
+def test_reconstruction(connection, full_circuit):
+    drop_and_replace_tables(connection=connection, clean=True)
+    refresh_all_stored_procedures(connection=connection)
+    reset_database_id(connection, table_name='linked_circuit_test', large_buffer_value=100000)
+
+    circuit_batches = windowed_cirq_to_pandora(circuit=full_circuit,
+                                               window_size=2,
+                                               is_test=True)
+    qubit_dict = dict((str(qubit), i) for i, qubit in enumerate(sorted(full_circuit.all_qubits(),
+                                                                       key=lambda q: str(q))))
+    for (batch, _) in circuit_batches:
+        insert_single_batch(connection=connection,
+                            batch=batch,
+                            is_test=True,
+                            table_name='linked_circuit_test')
+
+    extracted_circuit = extract_cirq_circuit(connection=connection,
+                                             circuit_label='x',
+                                             remove_io_gates=True,
+                                             table_name='linked_circuit_test',
+                                             original_qubits_test=qubit_dict,
+                                             just_count=False)
+
+    init_circuit = remove_measurements(remove_classically_controlled_ops(full_circuit))
+    extracted_circuit = remove_measurements(remove_classically_controlled_ops(extracted_circuit))
+
+    # use the same qubits for both circuits
+    qubit_map = dict(
+        zip(
+            sorted(init_circuit.all_qubits()),
+            sorted(extracted_circuit.all_qubits())
+        )
+    )
+    init_circuit = init_circuit.transform_qubits(qubit_map=qubit_map)
+
+    init_ops = list(init_circuit.all_operations())
+    final_ops = list(extracted_circuit.all_operations())
+    assert len(init_ops) == len(final_ops)
+
+    for (op_1, op_2) in zip(init_ops, final_ops):
+        assert op_1.gate.__class__.__name__ == op_2.gate.__class__.__name__
+
 def test_qualtran_adder_reconstruction(connection):
-    # append i, op_count, time for cirq_to_db and time for db_to_cirq
-
-    cirq_gate_set_test = cirq.Gateset(
-        cirq.Rz, cirq.Rx, cirq.Ry,
-        cirq.MeasurementGate, cirq.ResetChannel,
-        cirq.ZPowGate, cirq.XPowGate, cirq.YPowGate, cirq.HPowGate,
-        cirq.CZPowGate, cirq.CXPowGate,
-        cirq.X, cirq.Y, cirq.Z)
-
     for n_bits in range(2, 30):
         print(f'Adder test {n_bits}')
-
-        drop_and_replace_tables(connection=connection, clean=True)
-        refresh_all_stored_procedures(connection=connection)
-        reset_database_id(connection, table_name='linked_circuit_test', large_buffer_value=100000)
-
-        full_adder_circuit = get_adder_as_cirq_circuit(n_bits=n_bits)
-
-        qubit_dict = dict((str(qubit), i) for i, qubit in enumerate(sorted(full_adder_circuit.all_qubits(),
-                                                                           key=lambda q: str(q))))
-        adder_batches = windowed_cirq_to_pandora(circuit=full_adder_circuit,
-                                                 window_size=2,
-                                                 is_test=True)
-
-        for i, (batch, _) in enumerate(adder_batches):
-            insert_single_batch(connection=connection,
-                                batch=batch,
-                                is_test=True,
-                                table_name='linked_circuit_test')
-
-        extracted_circuit: cirq.Circuit = extract_cirq_circuit(connection=connection,
-                                                               circuit_label='x',
-                                                               remove_io_gates=True,
-                                                               table_name='linked_circuit_test',
-                                                               original_qubits_test=qubit_dict,
-                                                               just_count=False)
-
-        init_circuit = remove_measurements(remove_classically_controlled_ops(full_adder_circuit))
-        extracted_circuit = remove_measurements(remove_classically_controlled_ops(extracted_circuit))
-
-        # use the same qubits for both circuits
-        qubit_map = dict(
-            zip(
-                sorted(init_circuit.all_qubits()),
-                sorted(extracted_circuit.all_qubits())
-            )
-        )
-        init_circuit = init_circuit.transform_qubits(qubit_map=qubit_map)
-
-        init_ops = list(init_circuit.all_operations())
-        final_ops = list(extracted_circuit.all_operations())
-        assert len(init_ops) == len(final_ops)
-
-        for i, (op_1, op_2) in enumerate(zip(init_ops, final_ops)):
-            assert op_1.gate.__class__.__name__ == op_2.gate.__class__.__name__
-
+        full_circuit = get_adder_as_cirq_circuit(n_bits=n_bits)
+        test_reconstruction(connection=connection, full_circuit=full_circuit)
         print(f'Passed adder({n_bits})!')
 
 
 def test_qualtran_qrom_reconstruction(connection):
-    # append i, time for cirq_to_db and time for db_to_cirq
     times = []
     for i in range(3, 30):
         print(f'QROM test {i}')
         data = [*range(1, i)]
-
-        drop_and_replace_tables(connection=connection, clean=True)
-        refresh_all_stored_procedures(connection=connection)
-        reset_database_id(connection, table_name='linked_circuit_test', large_buffer_value=100000)
-
-        full_qrom_circuit = get_qrom_as_cirq_circuit(data=data)
-        qrom_batches = windowed_cirq_to_pandora(circuit=full_qrom_circuit,
-                                                window_size=2,
-                                                is_test=True)
-
-        qubit_dict = dict((str(qubit), i) for i, qubit in enumerate(sorted(full_qrom_circuit.all_qubits(),
-                                                                           key=lambda q: str(q))))
-        for i, (batch, _) in enumerate(qrom_batches):
-            insert_single_batch(connection=connection,
-                                is_test=True,
-                                batch=batch,
-                                table_name='linked_circuit_test')
-
-        extracted_circuit: cirq.Circuit = extract_cirq_circuit(connection=connection,
-                                                               circuit_label='x',
-                                                               remove_io_gates=True,
-                                                               table_name='linked_circuit_test',
-                                                               original_qubits_test=qubit_dict,
-                                                               just_count=False)
-
-        init_circuit = remove_measurements(remove_classically_controlled_ops(full_qrom_circuit))
-        extracted_circuit = remove_measurements(remove_classically_controlled_ops(extracted_circuit))
-
-        # use the same qubits for both circuits
-        qubit_map = dict(
-            zip(
-                sorted(init_circuit.all_qubits()),
-                sorted(extracted_circuit.all_qubits())
-            )
-        )
-        init_circuit = init_circuit.transform_qubits(qubit_map=qubit_map)
-
-        init_ops = list(init_circuit.all_operations())
-        final_ops = list(extracted_circuit.all_operations())
-
-        print(len(init_ops), len(final_ops))
-
-        assert len(init_ops) == len(final_ops)
-
-        for i, (op_1, op_2) in enumerate(zip(init_ops, final_ops)):
-            assert op_1.gate.__class__.__name__ == op_2.gate.__class__.__name__
-
+        full_circuit = get_qrom_as_cirq_circuit(data=data)
+        test_reconstruction(connection=connection, full_circuit=full_circuit)
         print(f'Passed qrom({i})!')
 
     return times
