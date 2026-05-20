0d3cc0aa56870bddaa7533252bf057609d068c07
Leo <leo.niemikallio@gmail.com>
2026-05-20 15:16:15 +0300

    Add qiskit support for X/Y/Z pow gates

--- tests/qiskit_translator_test.py
+++ tests/qiskit_translator_test.py
@@ -33,3 +33,40 @@ def test_qiskit_rotation_gates_keep_their_value():
         wrapper = PandoraGateWrapper(gate)
         qgate = wrapper.to_qiskit_gate()
         assert qgate.params[0] == math.pi
+
+
+def test_qiskit_exponent_gates_work():
+    circuit = QuantumCircuit(4)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.cz(1, 2)
+    circuit.ccx(0, 1, 2)
+
+    translator = QiskitToPandoraTranslator()
+    translated = [translator.translate(instruction) for instruction in circuit.data]
+
+    back_to_qiskit = [PandoraGateWrapper(gate).to_qiskit_gate() for gate in translated]
+
+    assert [gate.name for gate in back_to_qiskit] == [
+        "h",
+        "cx",
+        "cz",
+        "ccx",
+    ]
+
+def test_qiskit_exponent_gates_have_param():
+    circuit = QuantumCircuit(4)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    circuit.cz(1, 2)
+    circuit.ccx(0, 1, 2)
+
+    translator = QiskitToPandoraTranslator()
+    translated = [translator.translate(instruction) for instruction in circuit.data]
+
+    assert [(gate.type, gate.param) for gate in translated] == [
+        (PandoraGateTranslator.HPowGate.value, 1.0),
+        (PandoraGateTranslator.CXPowGate.value, 1.0),
+        (PandoraGateTranslator.CZPowGate.value, 1.0),
+        (PandoraGateTranslator.CCXPowGate.value, 1.0),
+    ]
